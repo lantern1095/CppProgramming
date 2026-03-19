b34fd8e46ba69e965151f069d8d4d3be323ddecb
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-03-19 18:40:08 +0900

    Update ch03_02_2.cpp

--- ch03/ch03_2/Task2/ch03_02_2.cpp
+++ ch03/ch03_2/Task2/ch03_02_2.cpp
@@ -1,1 +1,37 @@
+// **********************************************
+// 제 목 : 삼각형의 면적 구하기
+// 날 짜 : 2026년 3월18일
+// 작성자 : 2301377 김예찬
+// **********************************************
 
+#include <iostream>
+using namespace std;
+
+class Triangle {
+	int base;
+	int height;
+
+public:
+	Triangle();
+	Triangle(int a, int b);
+
+	double getArea();
+};
+Triangle::Triangle() : Triangle(1,1){}
+Triangle::Triangle(int a, int b) : base(a), height(b){}
+
+double Triangle::getArea(){
+	return 0.5 * base * height;
+}
+
+
+int main() {
+	Triangle tri1;
+	cout << "삼각형의 면적은 " << tri1.getArea() << endl;
+	
+	Triangle tri2(2, 4);
+	cout << "삼각형의 면적은 " << tri2.getArea() << endl;
+
+	return 0;
+
+}
