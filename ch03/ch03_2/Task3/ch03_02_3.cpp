ea5b4ab2f7097b540d7a434ac3decd5e3b6f6bca
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-03-19 18:41:42 +0900

    Update ch03_02_3.cpp

--- ch03/ch03_2/Task3/ch03_02_3.cpp
+++ ch03/ch03_2/Task3/ch03_02_3.cpp
@@ -1,1 +1,36 @@
 
+// **********************************************
+// 제 목 : 구의 부피 구하기
+// 날 짜 : 2026년 3월18일
+// 작성자 : 2301377 김예찬
+// **********************************************
+
+#include <iostream>
+using namespace std;
+
+class Sphere {
+	int radius;
+
+public:
+	Sphere();
+	Sphere(int a);
+
+	double getVolume();
+};
+Sphere::Sphere() : Sphere(1) {}
+Sphere::Sphere(int a) : radius(a) {}
+
+double Sphere::getVolume() {
+	return (4.0 / 3.0) * 3.14 * (radius * radius * radius);
+}
+
+int main() {
+	Sphere sph1;
+	cout << "구의 부피는" << sph1.getVolume() << endl;
+	
+	Sphere sph2(3);
+	cout << "구의 부피는" << sph2.getVolume() << endl;
+
+
+	return 0;
+}
