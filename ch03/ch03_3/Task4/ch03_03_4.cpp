8282f23737c9f7cdb970ae3b5b5b95e37e0e76f5
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-03-23 16:26:20 +0900

    Update ch03_03_4.cpp

--- ch03/ch03_3/Task4/ch03_03_4.cpp
+++ ch03/ch03_3/Task4/ch03_03_4.cpp
@@ -1,1 +1,40 @@
 
+// **********************************************
+// 제 목 : 구의 부피를 구하고, 소멸자를 활용하여 객체의 메모리를 반납하는 프로그램
+// 날 짜 : 2026년 3월 23일
+// 작성자 : 2301377 김예찬
+// **********************************************
+
+#include <iostream>
+using namespace std;
+
+class Sphere {
+	int radius;
+
+public:
+	Sphere();
+	Sphere(int r);
+	~Sphere();
+	double getVolume();
+};
+
+Sphere::Sphere() : Sphere(1){}
+Sphere::Sphere(int r) : radius(r) {
+	cout << "반지름이 " << radius << " 인 구 생성" << endl;
+}
+Sphere::~Sphere() {
+	cout << "반지름이 " << radius << " 인 구 소멸" << endl;
+}
+
+double Sphere::getVolume() {
+	return (4 / 3) * 3.14 * (radius * radius * radius);
+}
+
+
+int main() {
+	Sphere sph1;
+	cout << "구의 부피는" << sph1.getVolume() << endl;
+	Sphere sph2(3);
+	cout << "구의 부피는" << sph2.getVolume () << endl;
+	return 0;
+}
