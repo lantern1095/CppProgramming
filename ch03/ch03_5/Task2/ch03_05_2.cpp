f72eca866d055326a3c133b1ad66dfbd516effde
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-03-31 13:14:29 +0900

    Update ch03_05_2.cpp

--- ch03/ch03_5/Task2/ch03_05_2.cpp
+++ ch03/ch03_5/Task2/ch03_05_2.cpp
@@ -13,16 +13,13 @@ private:
 	int height;
 
 public:
+
+	Triangle():Triangle(1,1) {}
+	Triangle(int w, int h):width(w), height(h){  cout << "폭 " << width << ", 높이 " << height << " 인 삼각형 생성" << endl; }
+	~Triangle() { cout << "폭 " << width << ", 높이 " << height << " 인 삼각형 소멸" << endl; }
 	void setWidth(int w) { width = w; }
 	void setHeight(int h) { height = h; }
 	double getArea() { return width * height / 2.0; }
-
-	Triangle():Triangle(1,1) {}
-	Triangle(int w, int h) { 
-		width = w; height = h; 
-		cout << "폭 " << width << ", 높이 " << height << " 인 삼각형 생성" << endl;
-	}
-	~Triangle() { cout << "폭 " << width << ", 높이 " << height << " 인 삼각형 소멸" << endl; }
 };
 
 int main() {
