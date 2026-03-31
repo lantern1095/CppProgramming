0a6f7ff22fe43c621ec62a571416a3c5e633588f
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-03-31 13:04:16 +0900

    Update ch03_05_3_5.cpp

--- ch03/ch03_5/Task3/ch03_05_3_5.cpp
+++ ch03/ch03_5/Task3/ch03_05_3_5.cpp
@@ -1,1 +1,43 @@
 
+// **********************************************
+// 제 목 : 클래스를 이용해 커피머신을 만들고, 남은 재료의 양을 표시하는 프로그램
+// 날 짜 : 2026년 3월30일
+// 작성자 : 2301377 김예찬
+// **********************************************
+
+#include<iostream>
+using namespace std;
+
+class CoffeeMachine {
+private:
+	int coffee;
+	int water;
+	int sugar;
+
+public:
+	CoffeeMachine():CoffeeMachine(5, 10, 6){}
+	CoffeeMachine(int c, int w, int s):coffee(c),water(w),sugar(s){}
+	~CoffeeMachine() {}
+
+	void drinkEspresso() { coffee -= 1; water -= 1; }
+	void drinkAmericano() { coffee -= 1; water -= 2; }
+	void drinkSugarCoffee() { coffee -= 1; water -= 2; sugar -= 1; }
+	void full() { coffee = water = sugar = 10; }
+	void show() { cout << "[머신 상태] 커피:" << coffee << "	물:" << water << "	설탕:" << sugar << endl; }
+};
+
+int main() {
+	CoffeeMachine java(5, 10, 6);
+
+	java.drinkEspresso();
+	java.show();
+	java.drinkAmericano();
+	java.show();
+	java.drinkSugarCoffee();
+	java.show();
+	java.full();
+	java.show();
+
+
+	return 0;
+}
