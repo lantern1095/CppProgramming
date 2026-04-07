aeade299eb4c907f7dbd116449c7b0d4eeb26195
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-04-07 22:28:28 +0900

    Update ch04_02_4.cpp

--- ch04/ch04_2/Task4/ch04_02_4.cpp
+++ ch04/ch04_2/Task4/ch04_02_4.cpp
@@ -1,1 +1,29 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+	double n;
+	double max = 0;
+	
+	cout << "입력할 실수의 개수를 입력하세요 : ";
+	cin >> n;
+
+	double* p = new double[n];
+	if (!p) {
+		cout << "메모리를 할당할 수 없습니다.";
+		return -1;
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << i + 1 << "번째 실수: ";
+		cin >> p[i];
+	}
+
+	for (int i = 0; i < n; i++)
+		if (max < p[i]) {
+			max = p[i];
+		}
+	delete[] p;
+	cout << "최대값은 " << max << "입니다." << endl;
+}
