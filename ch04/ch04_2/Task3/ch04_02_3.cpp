90b3c85cbbc5dd05a4d8868f385618d2132d6e16
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-04-07 22:06:13 +0900

    Update ch04_02_3.cpp

--- ch04/ch04_2/Task3/ch04_02_3.cpp
+++ ch04/ch04_2/Task3/ch04_02_3.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "입력할 정수의 개수를 입력하세요 : ";
+	int n;
+	cin >> n; 
+	if (n <= 0) return -1;
+	int* p = new int[n];
+	if (!p) {
+		cout << "메모리를 할당할 수 없습니다.";
+		return -1;
+	}
+	for (int i = 0; i < n; i++) {
+		cout << i + 1 << "번째 정수: ";
+		cin >> p[i];
+	}
+	int avg = 0;
+	for (int i = 0; i < n; i++) { avg += p[i]; }
+
+	delete[] p;
+	cout << "평균값은 " << avg / n << "입니다." << endl;
+
+}
 
