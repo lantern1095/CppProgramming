974bf765d7cfe2e46251bf8c3b55f21abedbecda
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-04-09 19:15:49 +0900

    Update ch04_03_4.cpp

--- ch04/ch04_3/Task4/ch04_03_4.cpp
+++ ch04/ch04_3/Task4/ch04_03_4.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
+using namespace std;
 
+class Triangle {
+private:
+	int width;
+	int height;
+
+public:
+	Triangle():Triangle(1,1){}
+	Triangle(int w, int h);
+	~Triangle() { cout << "밑변 " << width << " 높이 " << height << " 인 삼각형 소멸" << endl; }
+
+	double getArea();
+};
+
+Triangle::Triangle(int w, int h) {
+	width = w; height = h; 
+	cout << "밑변 " << width << " 높이 " << height << " 인 삼각형 생성" << endl;
+}
+
+inline double Triangle::getArea() {
+	return width * height / 2.0;
+}
+
+int main()
+{
+	Triangle* triArr = new Triangle[3]{ Triangle(1,1), Triangle(2,2), Triangle(4,4) };
+	if (!triArr) {
+		cout << "메모리를 할당할 수 없습니다.";
+		return -1;
+	}
+
+	Triangle* p = triArr;
+	for (int i = 0; i < 3; i++) {
+		cout << "삼각형" << i + 1 << " 의 면적은 " << (p+i)->getArea() << endl;
+	}
+
+	delete[] triArr;
+	return 0;
+}
