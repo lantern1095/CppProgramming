63a938e55f5284d03bda7d75f0e467c1162dff1a
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-04-18 00:09:12 +0900

    Update ch04_04_2.cpp

--- ch04/ch04_4/Task2/ch04_04_2.cpp
+++ ch04/ch04_4/Task2/ch04_04_2.cpp
@@ -1,1 +1,43 @@
 
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Insert {
+private:
+	string strLine;
+	string strIn;
+	int index;
+public:
+	Insert() { index = 1; }
+
+	bool input();
+};
+
+bool Insert::input(){
+	cout << "문자열 입력 : ";
+	getline(cin, strIn, '\n');
+
+	if (strIn == "quit") {
+		return false;
+	}
+	strLine += "<" + to_string(index) + ">";
+	strLine += strIn;
+
+	cout << "이어진 문자열 : " << strLine << endl;
+	index++;
+
+	return true;
+}
+
+
+int main() {
+	Insert str;
+
+	while (true) {
+		if (!str.input()) { break; }
+		else { str.input(); }
+	}
+
+	return 0;
+}
