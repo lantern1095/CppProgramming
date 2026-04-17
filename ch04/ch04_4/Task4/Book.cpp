43946f422d3cf2f942804f0045a18f41942a5028
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-04-18 01:04:59 +0900

    Update Book.cpp

--- ch04/ch04_4/Task4/Book.cpp
+++ ch04/ch04_4/Task4/Book.cpp
@@ -1,1 +1,79 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Book {
+private:
+	string title;
+	int price;
+public:
+	Book() { title = ""; price = 0; }
+	void set(string title, int price) { this->title = title; this->price = price; }
+	string getTitle() { return title; }
+	int getPrice() { return price; }
+};
+
+class Library {
+private:
+	string name; 
+	Book* p; 
+	int size; 
+	int next = 0; 
+public:
+	Library(string name, int size);
+	~Library();
+	void add(string name, int price); 
+	void show(); 
+	Book* mostExpensive();  
+};
+
+Library::Library(string name, int size) {
+	this->name = name;
+	this->size = size;
+	p = new Book[size];
+	if (!p) {
+		cout << "메모리를 할당할 수 없습니다." << endl;
+	}
+}
+
+Library::~Library() {
+	delete[] p;
+}
+
+void Library::add(string name, int price) {
+	p[next].set(name, price);
+	next++;
+}
+
+void Library::show() {
+	cout << name << "에 입고된 책은 다음 " << next << "권 입니다." << endl;
+	for (int i = 0; i < next; i++) {
+		cout << "[" << (p + i)->getTitle() << "]   ";
+	}
+	cout << endl;
+}
+
+Book* Library::mostExpensive() {
+	int max = 0;
+
+	for (int i = 0; i < size; i++) {
+		if ((p + i)->getPrice() > (p + max)->getPrice()) {
+			max = i;
+		}
+	}
+	return &p[max];
+}
+
+int main() {
+	Library* lib = new Library("한국도서관", 10);
+	lib->add("명품 C++", 30000);
+	lib->add("라즈베리파이", 34000);
+	lib->add("HTML5", 33000);
+	lib->show();
+
+	Book* b = lib->mostExpensive();
+	cout << "가장 비싼 책: " << b->getTitle() << "," << b->getPrice() << endl;
+	delete lib;
+
+	return 0;
+}
