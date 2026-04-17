67f80fa3cadafa4c05f97d1a624774c193c90620
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-04-18 00:57:21 +0900

    Create ObjectPrice.cpp

---
+++ ch04/ch04_4/Task4/ObjectPrice.cpp
@@ -0,0 +1,74 @@
+
+#include<iostream>
+using namespace std;
+
+
+class ObjectPrice {
+private:
+	int arrSize;
+	int* price;
+
+public:
+	ObjectPrice() :ObjectPrice(3) {}
+	ObjectPrice(int num);
+	~ObjectPrice() { delete[] price; }
+
+	void setPrice();
+	int mostExpensive();
+	int mostCheap();
+};
+
+
+
+ObjectPrice::ObjectPrice(int num) {
+	arrSize = num;
+	price = new int[arrSize];
+	if (!price) {
+		cout << "메모리를 할당할 수 없습니다.";
+	}
+
+}
+
+void ObjectPrice::setPrice() {
+	cout << "물품 " << arrSize << " 개의 가격 입력 >> ";
+	for (int i = 0; i < arrSize; i++) {
+		cin >> price[i];
+	}
+}
+
+int ObjectPrice::mostExpensive() {
+	int ind = 0;
+	for (int i = 0; i < arrSize; i++) {
+		if (price[ind] < price[i]) {
+			ind = i;
+		}
+	}
+	return price[ind];
+}
+int ObjectPrice::mostCheap() {
+	int ind = 0;
+	for (int i = 0; i < arrSize; i++) {
+		if (price[ind] > price[i]) {
+			ind = i;
+		}
+	}
+	return price[ind];
+}
+
+
+int main() {
+	int count = 0;
+
+	cout << "구입할 물품의 개수>> ";
+	cin >> count;
+
+	ObjectPrice obj(count);
+
+	obj.setPrice();
+
+	cout << "가장 싼 가격은 " << obj.mostCheap() << endl;
+	cout << "가장 비싼 가격은" << obj.mostExpensive() << endl;
+
+
+	return 0;
+}
