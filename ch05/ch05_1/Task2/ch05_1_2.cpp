e64c742ae6cdefe7af08e93380a4da5d0b17b980
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-05-06 09:44:48 +0900

    Update ch05_1_2.cpp

--- ch05/ch05_1/Task2/ch05_1_2.cpp
+++ ch05/ch05_1/Task2/ch05_1_2.cpp
@@ -1,1 +1,31 @@
 
+// **********************************************
+// 제 목 : 값에 의한 호출을 이용해 사전에서 가장 뒤에 나오는 문자열 찾기
+// 날 짜 : 2026년 5월 6일
+// 작성자 : 2301377 김예찬
+// **********************************************
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+string GetLatterString(string str1, string str2);
+
+int main() {
+	string s1("hello");
+	string s2("world");
+	string res;
+	res = GetLatterString(s1, s2);
+	cout << "사전에서 뒤에 나오는 문자열은 " << res << "입니다." << endl;
+
+	return 0;
+}
+
+string GetLatterString(string str1, string str2) {
+	string result;
+	
+	if (str1 > str2) { result = str1; }
+	else { result = str2; }
+
+	return result;
+}
