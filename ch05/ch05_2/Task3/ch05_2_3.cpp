11226d6621dc7182252de5b941360b5e2c25fc94
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-05-10 16:31:09 +0900

    Update ch05_2_3.cpp

--- ch05/ch05_2/Task3/ch05_2_3.cpp
+++ ch05/ch05_2/Task3/ch05_2_3.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
 
+void swap(int& a, int & b);
+int main(void) {
+	int x, y;
+	cout << "정수 x를 입력 하시오 : ";
+	cin >> x;
+	cout << "정수 y를 입력 하시오 : ";
+	cin >> y;
+
+	cout << "swap 함수 호출 전 x = " << x << ", y = " << y << endl;
+	swap(x, y);
+	cout << "swap 함수 호출 후 x = " << x << ", y = " << y << endl;
+
+	return 0;
+}
+void swap(int& a, int& b) {
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
