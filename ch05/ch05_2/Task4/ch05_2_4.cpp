2e81985ab17abfac86845e1930e77ad123192762
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-05-10 16:41:24 +0900

    Update ch05_2_4.cpp

--- ch05/ch05_2/Task4/ch05_2_4.cpp
+++ ch05/ch05_2/Task4/ch05_2_4.cpp
@@ -1,1 +1,24 @@
 
+#include<iostream>
+using namespace std;
+
+void get_parts(double num, int& integer, double& fact);
+int main(void) {
+	int i = 0;
+	double f = 0.0;
+	double num;
+
+	cout << "실수를 입력하시오 : ";
+	cin >> num;
+
+	get_parts(num, i, f);
+
+	cout << "정수부 : " << i << endl;
+	cout << "소수부 : " << f << endl;
+
+	return 0;
+}
+void get_parts(double num, int& integer, double& fact) {
+	integer = (int)num;
+	fact = num - integer;
+}
