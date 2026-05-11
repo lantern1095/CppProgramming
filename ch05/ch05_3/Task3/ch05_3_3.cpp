e292302a044665b6e793f3c2027a65c4b4dc736a
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-05-11 09:56:26 +0900

    Update ch05_3_3.cpp

--- ch05/ch05_3/Task3/ch05_3_3.cpp
+++ ch05/ch05_3/Task3/ch05_3_3.cpp
@@ -1,1 +1,23 @@
 
+#include<iostream>
+#include<string>
+using namespace std;
+
+void SwapString(string& str1, string& str2);
+int main() {
+	string s1("hello ");
+	string s2("world ");
+	cout << "교환 전 문자열 " << s1 << s2 << endl;
+
+	SwapString(s1, s2);
+
+	cout << "교환 후 문자열 " << s1 << s2 << endl;
+
+	return 0;
+}
+
+void SwapString(string& str1, string& str2) {
+	string tmp = str1;
+	str1 = str2;
+	str2 = tmp;
+}
