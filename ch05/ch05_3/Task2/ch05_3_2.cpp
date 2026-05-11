55754102a5150d45a5ec41db57d5520d368ca011
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-05-11 09:47:48 +0900

    Update ch05_3_2.cpp

--- ch05/ch05_3/Task2/ch05_3_2.cpp
+++ ch05/ch05_3/Task2/ch05_3_2.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+void Uppercase(string& str);
+int main() {
+	string s1("hello");
+	cout << "변환 전 문자열 " << s1 << endl;
+	
+	Uppercase(s1);
+
+	cout << "변환 후 문자열 " << s1 << endl;
+
+	return 0;
+}
+
+void Uppercase(string & str) {
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] >= 'a' && str[i] <= 'z') {
+			str[i] = str[i] - 32;
+		}
+	}
+}
