4fc2bab9b795edf99b19146c13a1c853ff62d1cd
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-05-14 23:36:21 +0900

    Update ch05_4_2.cpp

--- ch05/ch05_4/Task2/ch05_4_2.cpp
+++ ch05/ch05_4/Task2/ch05_4_2.cpp
@@ -1,66 +1,57 @@
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstring>
+#include <string>
 using namespace std;
 
-class MyStack {
-	int* p;
-	int size = 0;
-	int tos = 0;
+class Person { // Person 클래스 선언
+string name;
+int id;
+
 public:
-	MyStack() :MyStack(10){}
-	MyStack(int size);
-	MyStack(const MyStack& src);
-	~MyStack() { delete[] p; }
-	bool push(int n);
-	bool pop(int& n);
+Person(int id, const string& name); // 생성자
+Person(const Person& person); // 복사 생성자
+~Person(); // 소멸자
+void changeName(const string& name);
+void show() { cout << id << ',' << name << endl; }
 };
 
-MyStack::MyStack(int size){
-	this->size = size;
-	p = new int[size];
-	if (!p) {
-		cout << "메모리 할당 실패";
-	}
+Person::Person(int id, const string& name) {
+this->id = id;
+this->name = name;
 }
 
-MyStack::MyStack(const MyStack& src){
-	this->size = src.size;
-	this->tos = src.tos;
-	this->p = new int[this->size];
-	if (!this->p) {
-		cout << "메모리 할당 실패";
-	}
-
-	for (int i = 0; i < this->size; i++) {
-		this->p[i] = src.p[i];
-	}
+Person::Person(const Person& person) { 
+this->id = person.id; 
+this->name = person.name;
+cout << "복사 생성자 실행. 원본 객체의 이름 " << this->name << endl;
 }
 
-bool MyStack::push(int n){
-	if (tos == size) { return false; }
-	
-	p[tos++] = n;
-	return true;
+Person::~Person() {
 }
 
-bool MyStack::pop(int& n){
-	if (tos == 0) { return false; }
-
-	n = p[--tos];
-	return true;
+void Person::changeName(const string& name) { 
+if (name.length() > this->name.length()) return; 
+this->name = name;
 }
 
 int main() {
-	MyStack a(10);
-	a.push(10); a.push(20);
+Person father(1, "Kitae");
+Person daughter(father); 
 
-	MyStack b = a;
-	b.push(30);
+cout << "daughter 객체 생성 직후 ----" << endl;
 
-	int n;
-	a.pop(n);
-	cout << "스택 a에서 팝한 값" << n << endl;
-	b.pop(n);
-	cout << "스택 b에서 팝한 값" << n << endl;
+father.show(); 
+daughter.show();
 
-	return 0;
+daughter.changeName("Grace");
+
+cout << "daughter 이름을 Grace로 변경한 후 ----" << endl;
+
+father.show();
+daughter.show(); 
+
+return 0; 
 }
+
