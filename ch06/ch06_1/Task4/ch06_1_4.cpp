4511a54f6356e70004a97cac0d8575e2ece8723b
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-05-18 09:36:06 +0900

    Update ch06_1_4.cpp

--- ch06/ch06_1/Task4/ch06_1_4.cpp
+++ ch06/ch06_1/Task4/ch06_1_4.cpp
@@ -1,1 +1,30 @@
 
+#include<iostream>
+using namespace std;
+
+class Point3D {
+	int x, y, z;
+public:
+	Point3D(){ x = 0; y = 0; z = 0; }
+	Point3D(int x, int y, int z) { this->x = x; this->y = y; this->z = z; }
+	Point3D(int x) { this->x = x; y = 0; z = 0; }
+	Point3D(int x, int y) { this->x = x; this->y = y; z = 0; }
+
+	void show() { cout << "3차원 점의 좌표는 (" << x << "," << y << "," << z << ")" << endl; }
+};
+
+int main() {
+	Point3D p0;
+	p0.show();
+	Point3D p1(1);
+	p1.show();
+	Point3D p2(1, 2);
+	p2.show();
+	Point3D p3(1, 2, 3);
+	p3.show();
+
+
+	return 0;
+}
+
+
