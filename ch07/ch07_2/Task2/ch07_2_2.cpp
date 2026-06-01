3a2db09672ef56e8443e64ed6b36b85fca79456c
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-06-01 09:32:40 +0900

    Update ch07_2_2.cpp

--- ch07/ch07_2/Task2/ch07_2_2.cpp
+++ ch07/ch07_2/Task2/ch07_2_2.cpp
@@ -1,1 +1,34 @@
+#include<iostream>
+using namespace std;
 
+class Complex {
+	int real;
+	int img;
+public:
+	Complex(int r = 0, int i = 0) { this->real = r; this->img = i; }
+	void show();
+	Complex operator* (Complex input);
+};
+
+void Complex::show() {
+	if (img > 0) { cout << this->real << "+" << this->img << "j" << endl; }
+	else{ cout << this->real << this->img << "j" << endl; }
+}
+
+Complex Complex::operator*(Complex input) {
+	Complex temp;
+
+	temp.real = (this->real * input.real) - (this->img * input.img);
+	temp.img = (this->real * input.img) + (this->img * input.real);
+
+	return temp;
+}
+
+int main() {
+	Complex x(2, 3), y(-5, 10), sum;
+	sum = x * y;
+	cout << "두 복소수의 곱은 ";
+	sum.show();
+
+	return 0;
+}
