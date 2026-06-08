d36ea5ba8a4a06fb5873c2a7eb19940b8653a99a
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-06-08 09:18:50 +0900

    Update ch07_3_2.cpp

--- ch07/ch07_3/Task2/ch07_3_2.cpp
+++ ch07/ch07_3/Task2/ch07_3_2.cpp
@@ -1,1 +1,37 @@
 
+#include<iostream>
+using namespace std;
+
+class Complex {
+	int real;
+	int img;
+public:
+	Complex(int real, int img);
+	Complex& operator++ ();
+	void show();
+};
+
+Complex::Complex(int real, int img) {
+	this->real = real;
+	this->img = img;
+
+	if (img < 0) { cout << "복소수" << this->real << this->img << " 생성" << endl; }
+	else{ cout << "복소수" << this->real << "+" << this->img << " 생성" << endl; }
+}
+
+void Complex::show() {
+	if (img < 0) { cout << this->real << this->img << endl; }
+	else { cout << "복소수" << this->real << "+" << this->img << " 생성" << endl; }
+}
+
+Complex& Complex::operator++() {
+	real++;
+	img++;
+	return *this;
+}
+
+int main() {
+
+
+	return 0;
+}
