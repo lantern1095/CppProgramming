c9587c9057d585dab0c55770b68f70e6cb64747c
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-06-08 10:32:14 +0900

    Update ch07_3_3.cpp

--- ch07/ch07_3/Task3/ch07_3_3.cpp
+++ ch07/ch07_3/Task3/ch07_3_3.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+using namespace std;
 
+class Power {
+	int kick;
+	int punch;
+public:
+	Power(int kick = 0, int punch = 0) { this->kick = kick; this->punch = punch; }
+	void show();
+	friend Power operator* (int x, Power& op);
+};
+
+Power operator* (int x, Power& op) {
+	Power tmp = op;
+	tmp.kick = tmp.kick * x;
+	tmp.punch = tmp.punch * x;
+	return tmp;
+}
+
+void Power::show() {
+	cout << "kick = " << kick << ", punch = " << punch << endl;
+}
+
+int main() {
+	Power a(3, 5), b;
+	a.show();
+	b.show();
+	b = 2 * a;
+	a.show();
+	b.show();
+
+	return 0;
+}
