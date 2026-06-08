04791f57a1ab0dfe67ad516c5f84537ccee96f8c
김예찬(Yechan Kim) <149346492+lantern1095@users.noreply.github.com>
2026-06-08 10:25:04 +0900

    Update ch07_3_2.cpp

--- ch07/ch07_3/Task2/ch07_3_2.cpp
+++ ch07/ch07_3/Task2/ch07_3_2.cpp
@@ -1,4 +1,3 @@
-
 #include<iostream>
 using namespace std;
 
@@ -6,32 +5,46 @@ class Complex {
 	int real;
 	int img;
 public:
-	Complex(int real, int img);
-	Complex& operator++ ();
+	Complex(int real = 0, int img = 0);
+	friend Complex& operator++ (Complex& op);
+	friend Complex operator-- (Complex& op, int x);
 	void show();
 };
+
+Complex& operator++ (Complex& op) {
+	op.real++;
+	op.img++;
+	return op;
+}
+
+Complex operator-- (Complex& op, int x){
+	Complex tmp = op;
+	op.real--;
+	op.img = op.img - 2;
+	return tmp;
+}
 
 Complex::Complex(int real, int img) {
 	this->real = real;
 	this->img = img;
 
-	if (img < 0) { cout << "복소수" << this->real << this->img << " 생성" << endl; }
-	else{ cout << "복소수" << this->real << "+" << this->img << " 생성" << endl; }
+	if (img < 0) { cout << "복소수 " << this->real << this->img << "j 생성" << endl; }
+	else{ cout << "복소수 " << this->real << "+" << this->img << "j 생성" << endl; }
 }
 
 void Complex::show() {
-	if (img < 0) { cout << this->real << this->img << endl; }
-	else { cout << "복소수" << this->real << "+" << this->img << " 생성" << endl; }
-}
-
-Complex& Complex::operator++() {
-	real++;
-	img++;
-	return *this;
+	if (img < 0) { cout << this->real << this->img << "j" << endl; }
+	else { cout << this->real << "+" << this->img << "j" << endl; }
 }
 
 int main() {
-
+	Complex x(2, -3);
+	++x;
+	cout << "증가결과 ";
+	x.show();
+	x--;
+	cout << "감소결과 ";
+	x.show();
 
 	return 0;
 }
